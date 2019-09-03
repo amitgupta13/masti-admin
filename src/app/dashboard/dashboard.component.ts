713de8a778e59eee1185064ebc199bf224d28cf1
Amit Gupta <amit.gupta@daffodilsw.com>
2019-09-03 12:52:44 +0530

    nc

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,23 @@
 import { Component } from "@angular/core";
+import * as faker from "faker";
 
 @Component({
   selector: "app-dashboard",
   templateUrl: "./dashboard.component.html",
   styleUrls: ["./dashboard.component.css"]
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  people;
+
+  constructor() {
+    this.people = Array(100)
+      .fill(1)
+      .map(_ => {
+        return {
+          image: `https://robohash.org/${faker.name.findName()}`,
+          name: faker.name.findName(),
+          bio: faker.hacker.phrase()
+        };
+      });
+  }
+}
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from "@angular/core";
 import { DashboardComponent } from "./dashboard.component";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
+import { ScrollingModule } from "@angular/cdk/scrolling";
 import { DashboardRoutingModule } from "./dashboard-routing.module";
+import { ProductItemComponent } from "./product-item/product-item.component";
 
 @NgModule({
-  declarations: [DashboardComponent],
-  imports: [CommonModule, ReactiveFormsModule, DashboardRoutingModule]
+  declarations: [DashboardComponent, ProductItemComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    DashboardRoutingModule,
+    ScrollingModule
+  ]
 })
 export class DashboardModule {}
